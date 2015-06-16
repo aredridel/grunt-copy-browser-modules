838d35003f33a13d5344bc9e4414db62cb50a13a
Aria Stewart <aredridel@dinhe.net>
2015-06-16 13:59:24 -0400

    Make it so

---
+++ tasks/index.js
@@ -0,0 +1,33 @@
+/*───────────────────────────────────────────────────────────────────────────*\
+ │  Copyright (C) 2014 eBay Software Foundation                                │
+ │                                                                             │
+ │hh ,'""`.                                                                    │
+ │  / _  _ \  Licensed under the Apache License, Version 2.0 (the "License");  │
+ │  |(@)(@)|  you may not use this file except in compliance with the License. │
+ │  )  __  (  You may obtain a copy of the License at                          │
+ │ /,'))((`.\                                                                  │
+ │(( ((  )) ))    http://www.apache.org/licenses/LICENSE-2.0                   │
+ │ `\ `)(' /'                                                                  │
+ │                                                                             │
+ │   Unless required by applicable law or agreed to in writing, software       │
+ │   distributed under the License is distributed on an "AS IS" BASIS,         │
+ │   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  │
+ │   See the License for the specific language governing permissions and       │
+ │   limitations under the License.                                            │
+ \*───────────────────────────────────────────────────────────────────────────*/
+
+'use strict';
+var copyTo = require('copy-browser-modules');
+
+module.exports = function (grunt) {
+    grunt.registerTask('copy-browser-modules', 'Copy browser modules into a flat tree', function () {
+        var done = this.async();
+
+        var options = this.options({
+            root: process.cwd(),
+            dest: 'public/components/'
+        });
+
+        copyTo(options.root, options.dest).then(function () { done(); }).catch(done);
+    });
+};
