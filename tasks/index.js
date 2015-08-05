25b4146728a532d0c83367cdf678db0a63f04943
Aria Stewart <aredridel@dinhe.net>
2015-08-05 10:40:42 -0400

    Actually support docRoot

--- tasks/index.js
+++ tasks/index.js
@@ -6,13 +6,19 @@ module.exports = function dustjs(grunt) {
     grunt.registerMultiTask('copy-browser-modules', 'Copy browser modules from npm into a final place', function () {
         var options = this.options({
             root: process.cwd(),
-            dest: 'public/components/'
+            dest: 'components',
+            docRoot: 'public'
         }, this.data);
 
         var done = this.async();
 
-        copyBrowserTo(options.root, options.dest, function (pkg) {
-            grunt.verbose.ok('copied', pkg.name, 'to', options.dest);
+        copyBrowserTo({
+            src: options.root,
+            dest: options.dest,
+            docRoot: options.docRoot,
+            each: function (pkg) {
+                grunt.verbose.ok('copied', pkg.name, 'to', options.dest);
+            }
         }).then(function () {
             done();
         }).catch(done);
