70aa45770e07bde8b96301060d818940ea32aa1f
Aria Stewart <aredridel@dinhe.net>
2015-08-05 10:43:50 -0400

    Resolve the destination relative to the docroot, rather than assuming relatedness

--- tasks/index.js
+++ tasks/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var copyBrowserTo = require('copy-browser-modules');
+var path = require('path');
 
 module.exports = function dustjs(grunt) {
     grunt.registerMultiTask('copy-browser-modules', 'Copy browser modules from npm into a final place', function () {
         var options = this.options({
             root: process.cwd(),
-            dest: 'components',
+            dest: 'public/components',
             docRoot: 'public'
         }, this.data);
 
@@ -14,7 +15,7 @@ module.exports = function dustjs(grunt) {
 
         copyBrowserTo({
             src: options.root,
-            dest: options.dest,
+            dest: path.relative(options.docRoot, options.dest),
             docRoot: options.docRoot,
             each: function (pkg) {
                 grunt.verbose.ok('copied', pkg.name, 'to', options.dest);
