0a5fd1a69ee5465c94cb7b9786643c1cdd138210
Aria Stewart <aredridel@dinhe.net>
2015-08-05 17:13:47 -0400

    Let us call it basePath instead of docRoot

--- tasks/index.js
+++ tasks/index.js
@@ -8,15 +8,15 @@ module.exports = function dustjs(grunt) {
         var options = this.options({
             root: process.cwd(),
             dest: 'public/components',
-            docRoot: 'public'
+            basePath: 'public'
         }, this.data);
 
         var done = this.async();
 
         copyBrowserTo({
             src: options.root,
-            dest: path.relative(options.docRoot, options.dest),
-            docRoot: options.docRoot,
+            dest: path.relative(options.basePath, options.dest),
+            basePath: options.basePath,
             each: function (pkg) {
                 grunt.verbose.ok('copied', pkg.name, 'to', options.dest);
             }
